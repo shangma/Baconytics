52e6ad0949f6aee01498e63b3d3a4a0c602eb491
neer300 <neer@neer-Lenovo-G470>
2012-03-07 08:44:33 -0500

    New method getBundleSetSize

--- src/edu/gatech/cc/Baconytics/appengine/DataModel/GAEKeyword.java
+++ src/edu/gatech/cc/Baconytics/appengine/DataModel/GAEKeyword.java
@@ -12,64 +12,68 @@ import com.google.appengine.api.datastore.Key;
 @PersistenceCapable(detachable = "true")
 public class GAEKeyword {
 
-    @PrimaryKey
-    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
-    private Key key;
+	@PrimaryKey
+	@Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+	private Key key;
 
-    @Persistent
-    private String keyword;
+	@Persistent
+	private String keyword;
 
-    @Persistent(mappedBy = "keyword")
-    private HashSet<GAEBundle> bundleSet;
+	@Persistent(mappedBy = "keyword")
+	private HashSet<GAEBundle> bundleSet;
 
-    public GAEKeyword(String keyword) {
-        this.keyword = keyword;
-        bundleSet = new HashSet<GAEBundle>();
-    }
+	public GAEKeyword(String keyword) {
+		this.keyword = keyword;
+		bundleSet = new HashSet<GAEBundle>();
+	}
 
-    public GAEKeyword(String keyword, HashSet<GAEBundle> bundleSet) {
-        this.keyword = keyword;
-        this.bundleSet = bundleSet;
-    }
+	public GAEKeyword(String keyword, HashSet<GAEBundle> bundleSet) {
+		this.keyword = keyword;
+		this.bundleSet = bundleSet;
+	}
 
-    public void setKey(Key key) {
-        this.key = key;
-    }
+	public void setKey(Key key) {
+		this.key = key;
+	}
 
-    public Key getKey() {
-        return key;
-    }
+	public Key getKey() {
+		return key;
+	}
 
-    public void setKeyword(String keyword) {
-        this.keyword = keyword;
-    }
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
 
-    public String getKeyword() {
-        return keyword;
-    }
+	public String getKeyword() {
+		return keyword;
+	}
 
-    public void setRedditList(HashSet<GAEBundle> bundleSet) {
-        this.bundleSet = bundleSet;
-    }
+	public void setRedditList(HashSet<GAEBundle> bundleSet) {
+		this.bundleSet = bundleSet;
+	}
 
-    public HashSet<GAEBundle> getBundleSet() {
-        return bundleSet;
-    }
+	public HashSet<GAEBundle> getBundleSet() {
+		return bundleSet;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        return obj != null && obj instanceof GAEKeyword
-                && this.key.equals(((GAEKeyword) obj).getKey());
-    }
+	public int getBundleSetSize() {
+		return bundleSet.size();
+	}
 
-    @Override
-    public String toString() {
-        StringBuilder ret = new StringBuilder();
-        ret.append("Keyword: ").append(getKeyword()).append(" Size: ")
-                .append(bundleSet.size()).append("\n");
-        for (GAEBundle bundle : bundleSet) {
-            ret.append("\t" + bundle.toString());
-        }
-        return ret.toString();
-    }
+	@Override
+	public boolean equals(Object obj) {
+		return obj != null && obj instanceof GAEKeyword
+				&& this.key.equals(((GAEKeyword) obj).getKey());
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder ret = new StringBuilder();
+		ret.append("Keyword: ").append(getKeyword()).append(" Size: ")
+				.append(bundleSet.size()).append("\n");
+		for (GAEBundle bundle : bundleSet) {
+			ret.append("\t" + bundle.toString());
+		}
+		return ret.toString();
+	}
 }
