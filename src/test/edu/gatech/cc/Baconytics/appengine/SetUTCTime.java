010d651cb28d35acdac9a62e1e3a2f7bda82b22b
neer300 <neer@neer-Lenovo-G470>
2012-03-07 09:03:40 -0500

    change

--- src/test/edu/gatech/cc/Baconytics/appengine/SetUTCTime.java
+++ src/test/edu/gatech/cc/Baconytics/appengine/SetUTCTime.java
@@ -16,43 +16,43 @@ import edu.gatech.cc.Baconytics.appengine.DataModel.UTCTime;
 @SuppressWarnings("serial")
 public class SetUTCTime extends HttpServlet {
 
-    private static PrintWriter writer;
+	private static PrintWriter writer;
 
-    @Override
-    public void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws IOException {
-        writer = resp.getWriter();
-        long newTime = -1;
-        try {
-            PersistenceManager pm = PMF.get().getPersistenceManager();
-            String param = req.getParameter("time");
-            if (param == null) {
-                UTCTime time = new UTCTime();
-                time.setTime(11111);
-                pm.makePersistent(time);
-                pm.close();
-                writer.println("Set UTC Time: " + time.getTime());
-                return;
-            }
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		writer = resp.getWriter();
+		long newTime = -1;
+		try {
+			PersistenceManager pm = PMF.get().getPersistenceManager();
+			String param = req.getParameter("time");
+			if (param == null) {
+				UTCTime time = new UTCTime();
+				time.setTime(0);
+				pm.makePersistent(time);
+				pm.close();
+				writer.println("Set UTC Time: " + time.getTime());
+				return;
+			}
 
-            newTime = Long.parseLong(param);
-            pm = PMF.get().getPersistenceManager();
-            Query query = pm.newQuery(UTCTime.class);
-            @SuppressWarnings("unchecked")
-            List<UTCTime> results = (List<UTCTime>) query.execute();
-            if (!results.isEmpty()) {
-                for (UTCTime e : results) {
-                    writer.println("Old Time: " + e.getTime());
-                    e.setTime(newTime);
-                    writer.println("New Time: " + e.getTime());
-                    pm.close();
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
+			newTime = Long.parseLong(param);
+			pm = PMF.get().getPersistenceManager();
+			Query query = pm.newQuery(UTCTime.class);
+			@SuppressWarnings("unchecked")
+			List<UTCTime> results = (List<UTCTime>) query.execute();
+			if (!results.isEmpty()) {
+				for (UTCTime e : results) {
+					writer.println("Old Time: " + e.getTime());
+					e.setTime(newTime);
+					writer.println("New Time: " + e.getTime());
+					pm.close();
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
 
-        }
+		}
 
-    }
+	}
 }
