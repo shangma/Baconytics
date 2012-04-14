eba13ec614263f029ecbec9bc40233f582664167
michaelfy <feng.yi.pg@gmail.com>
2012-04-14 16:29:04 -0400

    fixed error in Show.java

--- src/test/edu/gatech/cc/baconytics/aggregator/Show.java
+++ src/test/edu/gatech/cc/baconytics/aggregator/Show.java
@@ -2,7 +2,6 @@ package test.edu.gatech.cc.baconytics.aggregator;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Time;
 import java.util.List;
 
 import javax.jdo.PersistenceManager;
@@ -11,7 +10,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import edu.gatech.cc.baconytics.aggregator.AggregatorServlet;
 import edu.gatech.cc.baconytics.model.KeywordLinkMap;
 import edu.gatech.cc.baconytics.model.LinkKeywordMap;
 import edu.gatech.cc.baconytics.model.PMF;
@@ -27,9 +25,6 @@ public class Show extends HttpServlet {
             throws IOException {
 
         writer = resp.getWriter();
-        Time tm = new Time((new AggregatorServlet()).fetchLastUTCTime()
-                .getTime());
-        writer.println("Last update: " + tm.toString() + "<br/>");
         // Print all topics with keywords
         writer.println("============= Title -> Keyword =================<br/>");
         Query query = pm.newQuery(LinkKeywordMap.class);
