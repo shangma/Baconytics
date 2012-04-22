62c16de64cd843bfda949d296b2c2f87c79d1a0b
Brandon Headrick <hozuko@gmail.com>
2012-04-22 12:21:02 -0400

    fixed/added second graph to trend window; removed superfluous drop down menu

--- war/gchart.js
+++ war/gchart.js
@@ -1,9 +1,52 @@
     	
+    // Callback that creates and populates a data table,
+    // instantiates the pie chart, passes in the data and
+    // draws it.
+      function drawChart2() {
+
+	$.ajax({
+            url: "./keyentjsonservlet",
+            success: function (array) {
+                var obj = jQuery.parseJSON(array);
+                //console.log(obj.keyEnt);
+				var array_data = new Array();
+				var i = 0;
+				$.each(obj.keyEnt, function(key,value){
+					var keyword,score;
+					$.each(value, function(k,v){
+						if(k == 'keyword'){
+							keyword = v;
+						}
+						else if(k == 'score'){
+							score = v;
+						}
+					});
+					array_data[i] = new Array();
+					array_data[i][0] = keyword;
+					array_data[i][1] = score;
+					i++
+				});
+				console.log(array_data);
+                var data = new google.visualization.DataTable();
+                data.addColumn('string', 'keyword');
+                data.addColumn('number', 'score');
+                data.addRows(array_data);
+
+                var options = {
+                    'title': 'Trending topics',
+					'width': 400,
+					'height': 300
+                };
+
+                var chart = new google.visualization.PieChart(document.getElementById('graphRight'));
+                chart.draw(data, options);
+            }
+        });      
+      }
 
 
 
-
-	 function loadChart2(element) {
+     function loadChart2(element) {
 		google.load("visualization", "1", {packages:["corechart"]});
 		 
 		 
