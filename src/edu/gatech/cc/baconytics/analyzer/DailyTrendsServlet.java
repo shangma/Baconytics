3a06b7aa8085eb44edf4b0177a32990b01c5d008
michaelfy <feng.yi.pg@gmail.com>
2012-04-23 01:06:59 -0400

    Lessons learned  - I really underestimated the size of one week LinkStats, after tried weeklytrends on appengine, I gave up and switched to daily trends which is more acceptable  - added sample code of using dailytrends, see dailyTrend.html

--- src/edu/gatech/cc/baconytics/analyzer/DailyTrendsServlet.java
+++ src/edu/gatech/cc/baconytics/analyzer/DailyTrendsServlet.java
@@ -18,23 +18,28 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import com.google.appengine.api.datastore.Key;
 
+import edu.gatech.cc.baconytics.model.KeywordLinkMap;
+import edu.gatech.cc.baconytics.model.Link;
 import edu.gatech.cc.baconytics.model.LinkKeywordMap;
+import edu.gatech.cc.baconytics.model.LinkRelevanceBundle;
 import edu.gatech.cc.baconytics.model.LinkStats;
 import edu.gatech.cc.baconytics.model.PMF;
 import edu.gatech.cc.baconytics.model.UTCTime;
 import edu.gatech.cc.baconytics.model.WeeklyTrends;
 
 @SuppressWarnings("serial")
-public class WeeklyTrendsServlet extends HttpServlet {
+public class DailyTrendsServlet extends HttpServlet {
 
-    public static final long MILLISECOND_IN_ONE_DAY = 86400000L;
-    public static final int DAY_IN_ONE_WEEK = 7;
+    public static final long MILLISECOND_IN_ONE_HOUR = 3600000L;
     public static final String CURSORTYPE = "WEEKLY_TRENDS";
     public static final int THRESHHOLD = 10; // Only returns top 10 keywords
+    public static final int BATCHSIZE = 300;
+
     public static PrintWriter writer;
     public static int numOfProcessedLinkStats = 0;
     public static int numOfProcessedKeywords = 0;
@@ -47,6 +52,80 @@ public class WeeklyTrendsServlet extends HttpServlet {
         @Override
         public int compare(KeywordVotesBundle arg0, KeywordVotesBundle arg1) {
             return arg1.voteCount - arg0.voteCount;
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        writer = resp.getWriter();
+        PersistenceManager pm = PMF.get().getPersistenceManager();
+        try {
+            Query query = pm.newQuery(WeeklyTrends.class);
+            List<WeeklyTrends> results = (List<WeeklyTrends>) query.execute();
+            if (results.isEmpty()) {
+                return;
+            }
+            JSONArray dailyJsonArray = new JSONArray();
+            for (WeeklyTrends day : results) {
+                JSONObject dailyJsonObj = new JSONObject();
+                long startTime = day.getTimeStart();
+                long endTime = day.getTimeEnd();
+                dailyJsonObj.put("start_time", startTime);
+                dailyJsonObj.put("end_time", endTime);
+
+                JSONArray keywordListArray = new JSONArray();
+                ArrayList<Key> keyList = day.getKeywordLinkMapKeys();
+                ArrayList<Integer> voteList = day.getVoteList();
+                int size = keyList.size();
+                for (int i = 0; i < size; ++i) {
+                    Query keylinkQuery = pm.newQuery(KeywordLinkMap.class);
+                    keylinkQuery.setFilter("keyword == keywordParam");
+                    keylinkQuery.declareParameters("String keywordParam");
+                    List<KeywordLinkMap> klmResults = (List<KeywordLinkMap>) keylinkQuery
+                            .execute(keyList.get(i).getName());
+                    if (klmResults.isEmpty()) {
+                        continue;
+                    }
+                    KeywordLinkMap keywordLink = klmResults.get(0);
+                    JSONObject kwJson = new JSONObject();
+                    HashSet<LinkRelevanceBundle> krBundle = keywordLink
+                            .getBundleSet();
+                    JSONArray linkJsonArray = new JSONArray();
+                    for (LinkRelevanceBundle item : krBundle) {
+                        Query linkQuery = pm.newQuery(Link.class);
+                        linkQuery.setFilter("id == idParam");
+                        linkQuery.declareParameters("String idParam");
+
+                        List<Link> linkResults = (List<Link>) linkQuery
+                        // Use getName() to get the link's id
+                                .execute(item.getLinkKey().getName());
+                        Link link = linkResults.get(0);
+                        JSONObject linkJson = new JSONObject();
+                        linkJson.put("author", link.getAuthor());
+                        linkJson.put("domain", link.getDomain());
+                        linkJson.put("name", link.getName());
+                        linkJson.put("permalink", link.getPermalink());
+                        linkJson.put("title", link.getTitle());
+                        linkJson.put("url", link.getUrl());
+                        linkJson.put("subreddit", link.getSubreddit());
+                        linkJson.put("subredditId", link.getSubredditId());
+                        linkJsonArray.put(linkJson);
+                    }
+                    kwJson.put("keyword", keywordLink.getKeyword());
+                    kwJson.put("score", voteList.get(i));
+                    kwJson.put("link_list", linkJsonArray);
+                    keywordListArray.put(kwJson);
+                }
+                dailyJsonObj.put("keyword_list", keywordListArray);
+                dailyJsonArray.put(dailyJsonObj);
+            }
+            writer.println(dailyJsonArray.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            pm.close();
         }
     }
 
@@ -119,7 +198,7 @@ public class WeeklyTrendsServlet extends HttpServlet {
             lkQuery.declareParameters("String idParam");
             List<LinkKeywordMap> lkmResults = (List<LinkKeywordMap>) lkQuery
                     .execute(redditId);
-            if (lkmResults.isEmpty()) {
+            if (lkmResults == null || lkmResults.isEmpty()) {
                 continue;
             }
             // There should be only one.
@@ -146,19 +225,25 @@ public class WeeklyTrendsServlet extends HttpServlet {
     @SuppressWarnings("unchecked")
     private void fetchOneWeekLinkStats() throws Exception {
         UTCTime utcTimeObj = UTCTime.fetchLastUTCTime(CURSORTYPE);
-        PersistenceManager pm = PMF.get().getPersistenceManager();
         long timeStart = utcTimeObj.getTime();
         weekStart = timeStart;
-        long timeEnd = timeStart + MILLISECOND_IN_ONE_DAY;
+        long timeEnd = timeStart + MILLISECOND_IN_ONE_HOUR;
 
         // Doing fetching in batches to avoid timeout error
-        for (int i = 0; i < DAY_IN_ONE_WEEK; ++i) {
+        // for (int i = 0; i < 7; ++i) {
+        for (int j = 0; j < 24; ++j) {
+            PersistenceManager pm = PMF.get().getPersistenceManager();
+            // Due to GAE limitation, here we only get the 300 LinkStats
+            // Need a better way to solve it
             Query query = pm.newQuery("SELECT FROM "
                     + LinkStats.class.getName() + " WHERE timeSeen <= "
-                    + timeStart + " && timeSeen < " + timeEnd);
+                    + timeStart + " && timeSeen < " + timeEnd
+                    + " ORDER BY timeSeen DESC RANGE 0, " + BATCHSIZE);
+
             List<LinkStats> results = (List<LinkStats>) query.execute();
             if (!results.isEmpty()) {
-                // Go through each LinkStats, and fill in a keyword->votes Map.
+                // Go through each LinkStats, and fill in a keyword->votes
+                // Map.
                 // Only added the latest ones' votes to the map
                 for (LinkStats item : results) {
                     ++numOfProcessedLinkStats;
@@ -183,9 +268,10 @@ public class WeeklyTrendsServlet extends HttpServlet {
                 }
             }
             timeStart = timeEnd;
-            timeEnd = timeStart + MILLISECOND_IN_ONE_DAY;
+            timeEnd = timeStart + MILLISECOND_IN_ONE_HOUR;
+            pm.close();
         }
-        pm.close();
+        // }
         weekEnd = timeEnd;
         UTCTime.commitLastUTCTime(weekEnd, CURSORTYPE);
     }
